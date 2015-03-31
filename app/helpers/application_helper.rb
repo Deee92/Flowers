0684bf27e28c883d4b27b0f263a208ba2a4c41d9
Deepika Tiwari <deepikatiwari92@gmail.com>
2015-03-31 12:09:05 +0530

    Add helper method to set title of pages
    Related tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	# Provides the title for each page.
+	def full_title(page_title = '')
+		base_title = "Flowers"
+		if page_title.empty?
+			base_title
+		else
+			"#{page_title} | #{base_title}"
+		end
+	end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,22 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+	def setup
+		@base_title = "Flowers"
+	end
+
   test "should get home" do
     get :home
     assert_response :success
+    assert_template 'static_pages/home'
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get all" do
     get :all
     assert_response :success
+    assert_template 'static_pages/all'
+    assert_select "title", "All | #{@base_title}"
   end
 
 end
